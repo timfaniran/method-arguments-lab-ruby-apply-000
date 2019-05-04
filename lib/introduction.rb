f48385ff6ff0f99c1a4b62248cc048f92b042042
imperative-source-6800 <tim.faniran@gmail.com>
2019-05-04 19:39:29 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,4 +8,4 @@ def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language("Josh", "Ember.js")
+introduction_with_language("Dan", "Ember.js")
