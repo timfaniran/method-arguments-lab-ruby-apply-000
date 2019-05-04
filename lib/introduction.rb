63dc658bb30f17ac5c942894fcec28b78c9607e3
imperative-source-6800 <tim.faniran@gmail.com>
2019-05-04 19:49:08 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,10 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+introduction("Josh")
+
+def introduction_with_language(name_, language)
+  puts "Hi, my name is #{name_} and I am learning to program in #{language}."
+end
+
+introduction_with_language("Dan", "Ember.js")
